0519d9936a455ffdc6dcb2e9f36b4d31e81fd250
perpl777 <mllw327@gmail.com>
2024-03-21 21:57:58 +0700

    add adaptive for student-card

--- app/components/student/student-card.tsx
+++ app/components/student/student-card.tsx
@@ -9,19 +9,21 @@ import TechnologiesStudent from '@/app/components/student/technologies-student';
 
 const StudentCard = ({id_student}: any) => {
     return (
-        <div className='flex flex-row flex-wrap'>
-            <ImageStudent id_student={id_student}/>
+        <div className='grid grid-cols-2 max-lg:grid-cols-1'>
+            <div className='justify-self-end max-lg:justify-self-start'>
+                <ImageStudent id_student={id_student} />
+            </div>
 
-            <div className="bg-gray-50 flex flex-col flex-auto justify-between py-9 px-5">
+            <div className="bg-gray-50 flex flex-col flex-auto justify-between py-9 px-5 max-xl:w-[602px] max-lg:w-auto">
                 <div className='flex justify-end gap-2'>
                     <ContactsStudent id_student={id_student}/>
                 </div>
 
                 <div>
-                    <h1 className='titleName font-medium text-6xl'>
+                    <h1 className='titleName font-medium text-6xl max-sm:text-4xl'>
                         <NameStudent id_student={id_student} type='name_lastname'/>
                     </h1>
-                    <table className="table table-sm mt-12">
+                    <table className="table table-sm mt-12 max-sm:table-xs">
                         <tbody> 
                             <tr className='border-b border-black'> 
                                 <th className="p-0 text-xs font-normal text-gray-500 uppercase">Специальность</th>
